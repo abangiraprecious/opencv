85b4e3865d28f65c21c64c366fbdcd14ade19316
abangiraekyomuhendo <abangiraprecious@gmail.com>
2025-09-05 11:38:56 +0300

    jupyter

---
+++ geeksforgeeks/start.py
@@ -0,0 +1,7 @@
+import cv2 #for image reading and processng
+import numpy as np # for image array operation (used internally)
+import matplotlib.pyplot as plt # for displaying the image visually
+import os # for potentional path operations (though not directly used here)
+
+
+image_path = '/content/Sample_CV.jpg'
